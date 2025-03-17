94186654f2dbd935a19d091c4e52e9fa59c0e196
Trish0912 <trish0991@yahoo.in>
2025-03-17 14:01:52 +0530

    modified settings file

--- content_mgmnt/settings.py
+++ content_mgmnt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='bhjxasdyugasiugd87wgdiuqheg87')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
